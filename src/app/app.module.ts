689b2b759dd60238dc3030cc781552b1811f1331
Rafael Garcia <rafa@myariadna.com>
2018-01-12 18:30:09 +0100

    Second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { Keyboard } from '@ionic-native/keyboard';
+
+import { MyApp } from './app.component';
+import { DataProvider } from '../providers/data/data';
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    DataProvider,
+    Keyboard
+  ]
+})
+export class AppModule { }
---
+++ src/pages/home/home.ts
@@ -0,0 +1,98 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, AlertController, Platform } from 'ionic-angular';
+import { ChecklistModel } from '../../models/checklist-model';
+import { DataProvider } from '../../providers/data/data';
+import { Keyboard } from '@ionic-native/keyboard';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  checklists: ChecklistModel[] = []
+
+  constructor(public navCtrl: NavController, public dataService: DataProvider, public alertCtrl: AlertController, public platform: Platform, keyboard: Keyboard) {
+
+  }
+
+  ionViewDidLoad() {
+
+  }
+
+  addChecklist(): void {
+    let prompt = this.alertCtrl.create({
+      title: 'New checklist',
+      message: 'Enter the name of the new checklist below:',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            let newChecklist = new ChecklistModel(data.name, []);
+            this.checklists.push(newChecklist);
+            newChecklist.checklistUpdates().subscribe(update => {
+              this.save();
+            });
+            this.save();
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  renameChecklist(checklist): void {
+    let prompt = this.alertCtrl.create({
+      title: 'Rename Checklist',
+      message: 'Enter the new name of the checklist below:',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            let index = this.checklists.indexOf(checklist);
+            if (index > -1) {
+              this.checklists[index].setTitle(data.name);
+              this.save();
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  viewChecklist(checklist): void {
+    this.navCtrl.push('ChecklistPage',{
+      checklist: checklist
+    });
+  }
+
+  removeChecklist(checklist): void {
+    let index = this.checklists.indexOf(checklist);
+    if (index > -1) {
+      this.checklists.splice(index, 1);
+      this.save();
+    }
+  }
+
+  save(): void {
+
+  }
+}
